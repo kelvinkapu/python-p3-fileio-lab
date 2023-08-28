1251be22c23401471a2bb39d0ab0887100156ed5
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-08-28 21:45:41 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    # file_name = tmp_path / "test_file"
+    # file_content = "This is a test content."
+    with open(f'{file_name}.txt', 'w', encoding='utf-8') as file_name:
+        file_name.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a' , encoding='utf-8') as file_name:
+        file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt' , encoding='utf-8') as file_name:
+        # for line in file_name:
+            # print(line)
+        return file_name.read()
